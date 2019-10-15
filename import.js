c08500ea49c3f9a468774d04bfe7e578da6e6207
Yang Mingshan <y.mingshan3@gmail.com>
2019-10-15 16:38:51 +0800

    Add support for some ESLint plugins

---
+++ import.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'import/no-useless-path-segments': 'off',
+    'import/order': 'off',
+    'import/newline-after-import': 'off'
+  }
+};
---
+++ unicorn.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'unicorn/escape-case': 'off',
+    'unicorn/import-index': 'off',
+    'unicorn/new-for-builtins': 'off',
+    'unicorn/no-array-instanceof': 'off',
+    'unicorn/no-console-spaces': 'off',
+    'unicorn/no-hex-escape': 'off',
+    'unicorn/no-new-buffer': 'off',
+    'unicorn/no-zero-fractions': 'off',
+    'unicorn/number-literal-case': 'off',
+    'unicorn/prefer-spread': 'off',
+    'unicorn/prefer-type-error': 'off',
+    'unicorn/regex-shorthand': 'off',
+    'unicorn/throw-new-error': 'off'
+  }
+};
