9ba3bbd6b59acff8b1da48dfe23e911268e44af2
Yang Mingshan <y.mingshan3@gmail.com>
2019-10-16 19:04:46 +0800

    Add support for @typescript-eslint/eslint-plugin

---
+++ @typescript-eslint.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/brace-style': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/func-call-spacing': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-array-constructor': 'off',
+    '@typescript-eslint/no-extra-parens': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-unnecessary-qualifier': 'off',
+    '@typescript-eslint/no-unnecessary-type-arguments': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+    '@typescript-eslint/prefer-function-type': 'off',
+    '@typescript-eslint/prefer-namespace-keyword': 'off',
+    '@typescript-eslint/prefer-readonly': 'off',
+    '@typescript-eslint/quotes': 'off',
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/type-annotation-spacing': 'off'
+  }
+};
