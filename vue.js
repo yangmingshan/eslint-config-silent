c4c1d02731eddacf8dd24a8185daefc813b10228
Yang Mingshan <y.mingshan3@gmail.com>
2019-10-16 17:01:16 +0800

    Add support for eslint-plugin-vue

---
+++ vue.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'vue/attribute-hyphenation': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/html-indent': 'off',
+    'vue/html-quotes': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/mustache-interpolation-spacing': 'off',
+    'vue/name-property-casing': 'off',
+    'vue/no-multi-spaces': 'off',
+    'vue/no-spaces-around-equal-signs-in-attribute': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/v-bind-style': 'off',
+    'vue/v-on-style': 'off',
+    'vue/attributes-order': 'off',
+    'vue/order-in-components': 'off',
+    'vue/array-bracket-spacing': 'off',
+    'vue/arrow-spacing': 'off',
+    'vue/block-spacing': 'off',
+    'vue/brace-style': 'off',
+    'vue/comma-dangle': 'off',
+    'vue/component-name-in-template-casing': 'off',
+    'vue/dot-location': 'off',
+    'vue/key-spacing': 'off',
+    'vue/keyword-spacing': 'off',
+    'vue/object-curly-spacing': 'off',
+    'vue/script-indent': 'off',
+    'vue/space-infix-ops': 'off',
+    'vue/space-unary-ops': 'off'
+  }
+};
