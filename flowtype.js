a0af79cb82dded70f6d8098aa247bf8367089ac7
Yang Mingshan <y.mingshan3@gmail.com>
2019-10-17 13:19:20 +0800

    Add support for eslint-plugin-flowtype

---
+++ flowtype.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'flowtype/array-style-complex-type': 'off',
+    'flowtype/array-style-simple-type': 'off',
+    'flowtype/arrow-parens': 'off',
+    'flowtype/boolean-style': 'off',
+    'flowtype/delimiter-dangle': 'off',
+    'flowtype/generic-spacing': 'off',
+    'flowtype/object-type-delimiter': 'off',
+    'flowtype/require-indexer-name': 'off',
+    'flowtype/semi': 'off',
+    'flowtype/sort-keys': 'off',
+    'flowtype/space-after-type-colon': 'off',
+    'flowtype/space-before-generic-bracket': 'off',
+    'flowtype/space-before-type-colon': 'off',
+    'flowtype/type-import-style': 'off',
+    'flowtype/union-intersection-spacing': 'off'
+  }
+};
