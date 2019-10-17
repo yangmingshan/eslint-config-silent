29118e83627710c1ab6ca1648bde2ae07dffcb0f
Yang Mingshan <y.mingshan3@gmail.com>
2019-10-17 14:11:21 +0800

    Add support for eslint-plugin-ava

---
+++ ava.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'ava/no-incorrect-deep-equal': 'off',
+    'ava/no-inline-assertions': 'off',
+    'ava/no-only-test': 'off',
+    'ava/no-skip-test': 'off',
+    'ava/prefer-t-regex': 'off'
+  }
+};
