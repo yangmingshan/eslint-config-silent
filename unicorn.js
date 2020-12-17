aea6fcb3ff22a105f48466f299deb3960fab4ea1
Yang Mingshan <y.mingshan3@gmail.com>
2020-12-17 21:06:11 +0800

    Add `unicorn/empty-brace-spaces` `unicorn/no-lonely-if` `unicorn/prefer-date-now`

--- unicorn.js
+++ unicorn.js
@@ -2,15 +2,18 @@
 
 module.exports = {
   rules: {
+    'unicorn/empty-brace-spaces': 'off',
     'unicorn/escape-case': 'off',
     'unicorn/import-index': 'off',
     'unicorn/no-array-instanceof': 'off',
     'unicorn/no-console-spaces': 'off',
     'unicorn/no-hex-escape': 'off',
+    'unicorn/no-lonely-if': 'off',
     'unicorn/no-new-buffer': 'off',
     'unicorn/no-useless-undefined': 'off',
     'unicorn/no-zero-fractions': 'off',
     'unicorn/number-literal-case': 'off',
+    'unicorn/prefer-date-now': 'off',
     'unicorn/prefer-negative-index': 'off',
     'unicorn/prefer-optional-catch-binding': 'off',
     'unicorn/prefer-spread': 'off',
