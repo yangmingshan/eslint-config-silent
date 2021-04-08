92b912f6a096f68fb99d84e248d636004ecf7e07
Yang Mingshan <y.mingshan3@gmail.com>
2021-04-08 16:26:51 +0800

    Remove `ava/no-only-test` `ava/no-skip-test`

--- ava.js
+++ ava.js
@@ -4,8 +4,6 @@ module.exports = {
   rules: {
     'ava/no-incorrect-deep-equal': 'off',
     'ava/no-inline-assertions': 'off',
-    'ava/no-only-test': 'off',
-    'ava/no-skip-test': 'off',
     'ava/prefer-t-regex': 'off',
   },
 };
