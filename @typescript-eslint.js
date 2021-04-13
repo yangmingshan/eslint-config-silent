a3b12d6ac9dc4bb19d8d098fc7229d2a6283fa80
Yang Mingshan <y.mingshan3@gmail.com>
2021-04-13 23:53:14 +0800

    Add `@typescript-eslint/prefer-regexp-exec`

--- @typescript-eslint.js
+++ @typescript-eslint.js
@@ -28,6 +28,7 @@ module.exports = {
     '@typescript-eslint/prefer-namespace-keyword': 'off',
     '@typescript-eslint/prefer-readonly': 'off',
     '@typescript-eslint/prefer-reduce-type-parameter': 'off',
+    '@typescript-eslint/prefer-regexp-exec': 'off',
     '@typescript-eslint/promise-function-async': 'off',
     '@typescript-eslint/sort-type-union-intersection-members': 'off',
     '@typescript-eslint/object-curly-spacing': 'off',
